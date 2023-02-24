86a3fe041beebf198e4935fc695d9642f064e5b5
Adam Abdulkadir <78934570+aabdulk862@users.noreply.github.com>
2023-02-23 19:58:33 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# youtube-clone
+ Youtube Clone built with React, Material UI, and RapidAPI.
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/system";
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./components";
+
+const App = () => (
+  <div>
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />}></Route>
+          <Route path="/video/:id" element={<VideoDetail />}></Route>
+          <Route path="/channel/:id" element={<ChannelDetail />}></Route>
+          <Route path="/search/:searchTerm" element={<SearchFeed />}></Route>
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  </div>
+);
+
+export default App;
